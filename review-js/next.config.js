60bd8fac739b7962b5f71d2ed528d14cd9d71ddb
LeeJiHyeon <rachel2148072@gmail.com>
2023-03-15 11:37:30 +0900

    2.1 : redirect:url이동시키기/ rewrite:request경로를 source(프록시역할)로 마스킹하기

--- review-js/next.config.js
+++ review-js/next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = "08192788a8e5af154550148c8022e396";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirect() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
