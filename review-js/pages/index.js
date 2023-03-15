90fd0e1b05ce7f26441cd7f62f0aad0851201db2
LeeJiHyeon <rachel2148072@gmail.com>
2023-03-15 14:20:04 +0900

    ~2.8 : Catch all routes [...params], getServerSideProps로 req.parasm가져오기, 404 페이지만들기

--- review-js/pages/index.js
+++ review-js/pages/index.js
@@ -4,15 +4,7 @@ import { useRouter } from "next/router";
 export default function Home({ results }) {
   const router = useRouter();
   const handleNavigateMovieDetail = (id, title) => {
-    router.push(
-      {
-        pathname: `movies/${id}`,
-        query: {
-          title,
-        },
-      },
-      `movies/${id}`
-    );
+    router.push(`/movies/${title}/${id}`);
   };
   return (
     <div className="container">
@@ -24,14 +16,7 @@ export default function Home({ results }) {
           key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>
-            <Link
-              href={{
-                pathname: `/movies/${movie.id}`,
-                query: {
-                  title: movie.original_title,
-                },
-              }}
-              as={`/movies/${movie.id}`}>
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
               {movie.original_title}
             </Link>
           </h4>
--- review-js/pages/movies/[id].js
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function movieId() {
-  const router = useRouter();
-  console.log(router);
-  return <>detail</>;
-}
