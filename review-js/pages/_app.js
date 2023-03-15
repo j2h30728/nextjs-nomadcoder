b0692a5d565af534296d15f8b86ff11d53c2e1cb
LeeJiHyeon <rachel2148072@gmail.com>
2023-03-15 10:25:35 +0900

    review 2.1: layout/Header/ fetching data

--- review-js/pages/_app.js
+++ review-js/pages/_app.js
@@ -1,16 +1,16 @@
+import Layout from "@/components/Layout";
 import NavBar from "../components/NavBar";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
       <style jsx global>{`
         a {
           color: beige;
         }
       `}</style>
-    </>
+    </Layout>
   );
 }
--- review-js/pages/index.js
+++ review-js/pages/index.js
@@ -1,12 +1,29 @@
+import Seo from "@/components/Seo";
+import { useEffect, useState } from "react";
+
+const API_KEY = "08192788a8e5af154550148c8022e396";
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
+      <Seo title="Home" />
       <h1>Hello</h1>
-      <style jsx global>{`
-        a {
-          color: blue;
-        }
-      `}</style>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map(movie => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
